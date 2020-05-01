c8d4c4b8414a7b6d5b8e30647b7a181884507935
IoanaCarbune <ioanaelena.crb@gmail.com>
2020-05-01 16:45:21 +0300

    added checkout item component

--- src/App.js
+++ src/App.js
@@ -6,11 +6,14 @@ import './App.css';
 import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import CheckoutPage from './pages/checkout/checkout.component';
+
 import Header from './components/header/header.component';
 import {auth,createUserProfileDocument} from './firebase/firebase.utils';
+
 import {setCurrentUser} from './redux/user/user.actions';
-
-
+import {selectCurrentUser} from './redux/user/user.selectors';
+import {createStructuredSelector} from 'reselect';
 class App extends React.Component {
 
   componentDidMount(){
@@ -46,6 +49,7 @@ class App extends React.Component {
        <Switch>
          <Route exact path='/' component={Homepage}/>
          <Route  path='/shop' component={ShopPage}/>
+         <Route  exact path='/checkout' component={CheckoutPage}/>
          <Route exact  
          path='/signIn' 
          render={()=>
@@ -65,8 +69,8 @@ class App extends React.Component {
  
 }
 
-const mapStateToProps=({user})=>({
-  currentUser: user.currentUser
+const mapStateToProps=createStructuredSelector({
+  currentUser: selectCurrentUser
 })
 const mapDispatchToProps= dispatch=>({
   setCurrentUser:user=> dispatch(setCurrentUser(user))
